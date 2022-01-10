19401bcba6a33f16460a6cb3a91bd12e961aa70e
Argeu Bettoni <argeu.bettoni@gmail.com>
2022-01-10 13:45:07 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,22 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div>
+                    <div className="row">
+                        <div className="col-sm-6 col-lg-4 col-xl-3 md-3">
+                            <MovieCard />
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
